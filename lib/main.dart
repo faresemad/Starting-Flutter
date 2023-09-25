530a96daca508a1f26cec4441e4ba1653a20ee68
Fares Emad <faresemadx@gmail.com>
2023-09-25 20:04:55 +0200

    feat: :sparkles: used some `arthmetice operators`

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,12 @@
 void main() {
-  // Introduce yourself
-  String name = "Fares Emad";
-  int age = 22;
-  // ----------------------------------------------------------
-  // please don't say it's too low <3
-  double gpa = 3.5;
-  // ----------------------------------------------------------
-  // num can be either int or double -> 4 or 4.5
-  num rating = 4.5;
-  // ----------------------------------------------------------
-  // check if I'm a student or not (true or false)
-  bool isStudent = true;
-  // ----------------------------------------------------------
-  // dynamic can be anything (string, int, double, bool, ...)
-  dynamic anything = "I can be anything";
-  // dynamic anything2 = 5;
-  // dynamic anything3 = 5.5;
-  // dynamic anything4 = true;
-  // ----------------------------------------------------------
-  var variables = "I'm a string";
-  // var variables2 = 5;
-  // var variables3 = 5.5;
-  // var variables4 = true;
-  // var text;
-  // text -> the type of the variable is `dynamic` because it's not initialized
-  // ----------------------------------------------------------
-  print("Hello World, my name's $name, I'm $age years old, and my GPA is $gpa");
-  print("My rating is $rating");
-  print("Am I a student? $isStudent");
-  print(anything);
-  print(variables);
+  int a = 10;
+  int b = 20;
+  int c = a + b; // - > 10 + 20 = 30
+  // int c = a - b; // - > 10 - 20 = -10
+  // int c = a * b; // - > 10 * 20 = 200
+  // int c = a ~/ b; // - > 10 ~/ 20 = 0
+  // int c = a % b; // - > 10 % 20 = 10
+  // double c = a / b; // - > 10 / 20 = 0.5
+  // int c = a / b; // can't divide int by int we should use double
+  print(c);
 }
