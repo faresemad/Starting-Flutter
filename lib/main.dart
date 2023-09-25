652e86e0c7738dfe946d0641ef0cddf1d6db2e41
Fares Emad <faresemadx@gmail.com>
2023-09-25 13:23:24 +0200

    feat: :sparkles: using `bool` to store boolean type `true` or `false`

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   int age = 22;
   double gpa = 3.5; // please don't say it's too low <3
   num rating = 4.5; // num can be either int or double -> 4 or 4.5
+  bool isStudent = true; // check if I'm a student or not (true or false)
   print("Hello World, my name's $name, I'm $age years old, and my GPA is $gpa");
   print("My rating is $rating");
+  print("Am I a student? $isStudent");
 }
