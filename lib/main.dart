ab80ef5148708cc4facad40aa72c725e6e3c3f9e
Fares Emad <faresemadx@gmail.com>
2023-09-25 11:50:18 +0200

    Using `double` to store floating value like my `gpa`

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ void main() {
   // Introduce yourself
   String name = "Fares Emad";
   int age = 22;
-  print("Hello World, my name's $name, I'm $age years old");
+  double gpa = 3.5; // please don't say it's too low <3
+  print("Hello World, my name's $name, I'm $age years old, and my GPA is $gpa");
 }
