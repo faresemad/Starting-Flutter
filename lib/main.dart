60194c35d5e7fd14f484f48af76c2245160ad695
Fares Emad <faresemadx@gmail.com>
2023-09-25 13:12:47 +0200

    feat: :sparkles: using `num` to store any type of number
    num can be aither `int` or `double` like `4` or `4.5` but not recomment uses this type everytime

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,7 @@ void main() {
   String name = "Fares Emad";
   int age = 22;
   double gpa = 3.5; // please don't say it's too low <3
+  num rating = 4.5; // num can be either int or double -> 4 or 4.5
   print("Hello World, my name's $name, I'm $age years old, and my GPA is $gpa");
+  print("My rating is $rating");
 }
