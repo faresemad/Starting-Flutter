6c4d9fecc7f3879d8fa57f437e5b6d259847f9f7
Fares Emad <faresemadx@gmail.com>
2023-09-26 12:26:30 +0200

    feat: :sparkles: learn Collection data type (`list`)

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
 void main() {
-  String name = "Fares Emad";
-  print(name.toUpperCase()); // Output: "FARES EMAD"
-  int age = 22;
-  print(age.toDouble()); // Output: 22.0
-
-  // --------------------------------------------------------------
-  // also we can use toLowerCase(), toUpperCase(), trim(),
-  // trimLeft(), trimRight(), split(), replaceAll(), replaceFirst(),
-  // contains(), substring(), compareTo(), toInt(), toDouble(),
-  // toString(), isEmpty(), isNotEmpty(), length, indexOf(),
-  // lastIndexOf(), startsWith(), endsWith(), charAt(), codeUnitAt(),
-  // runes, padLeft(), padRight(), splitMapJoin(), replaceRange(),
-  // replaceAllMapped(), replaceFirstMapped(), splitMapJoin(),
-  // toLowerCase(), toUpperCase(), trim(), trimLeft(), trimRight(),
-  // split(), replaceAll(), replaceFirst(), contains(), substring(),
-  // compareTo(), toInt(), toDouble(), toString(), isEmpty(), isNotEmpty(),
-  // length, indexOf(), lastIndexOf(), startsWith(), endsWith(), charAt(),
-  // codeUnitAt(), runes, padLeft(), padRight(), splitMapJoin(),
-  // replaceRange(), replaceAllMapped(), replaceFirstMapped(), splitMapJoin()
-  // ------------------------------------------------------------
+  List<String> names = ["fares", "emad"];
+  // ['fares', 'emad', 1]
+  // The element type 'int' or any other type can't be assigned to the list type 'String'.
+  print(names[0]);
+  var d = ["fares", "emad", 1];
+  d.add("test");
+  print(d[3]);
 }
