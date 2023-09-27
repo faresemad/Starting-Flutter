69670aa22864024955d6d93437b905549f65896f
Fares Emad <faresemadx@gmail.com>
2023-09-28 01:08:30 +0200

    feat: :sparkles: understanding `null safety` and `null safety operators`

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 void main() {
-  int x = 0;
-  switch (x) {
-    case 1:
-      print('x is 1');
-      break;
-    case 2:
-      print('x is 2');
-      break;
-    default:
-      print('x is not 1 or 2');
-  }
+  int? number1;
+  int number2 = 10;
+  int result;
+
+  result = number1 ?? 15;
+  print(result); // 15
+  // int result = number1! + number2;
+  // print(result); // Exception has occurred
+
+  // print(number1?.toDouble());
+
+  // print(number1!.toDouble()); // Exception has occurred
 }
