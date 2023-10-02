acf9db0e28de562dcbb409b8bd56e6557cf3f301
Fares Emad <faresemadx@gmail.com>
2023-10-02 16:44:35 +0200

    make `human` as a external class and make a `numberOfArms` as a Private variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,10 @@
-void main() {
-  Humen fares = new Humen(
-    name: "Fares Emad",
-    age: 22,
-    height: 183,
-    weight: 60,
-  );
-  // fares.name = "Fares Emad";
-  // fares.age = 22;
-  // fares.height = 183;
-  // fares.weight = 60;
-  fares.introduce();
-}
+import 'human.dart';
 
-class Humen {
-  String? name;
-  int? age;
-  double? height;
-  double? weight;
-  int numberOfArms = 2;
-  // Constructor function (runs when the class is created)
-  Humen({String? name, int? age, double? height, double? weight}) {
-    print("A new human has been created!");
-    this.name = name;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-  }
-  void introduce() {
-    print(
-        "Hello, my name is $name and I'm $age years old.\nI'm $height cm tall and weigh $weight kg.");
-  }
+void main() {
+  Human person = Human(name: "John Doe", age: 30, height: 180.0, weight: 80.0);
+  person.introduce();
+  person.setnumberOfArms(3);
+  print("Number of arms: ${person.getnumberOfArms()}");
+  person.setnumberOfArms(1);
+  print("Number of arms: ${person.getnumberOfArms()}");
 }
