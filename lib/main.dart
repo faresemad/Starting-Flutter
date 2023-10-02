cdde196bb56e85dc03f78bbbd772e6cd02fca8ab
Fares Emad <faresemadx@gmail.com>
2023-10-02 13:25:22 +0200

    understanding `constructor` and `methods`

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   fares.age = 22;
   fares.height = 183;
   fares.weight = 60;
+  fares.introduce();
 }
 
 class Humen {
@@ -12,4 +13,12 @@ class Humen {
   double? height;
   double? weight;
   int numberOfArms = 2;
+  // Constructor function (runs when the class is created)
+  Humen() {
+    print("A new human has been created!");
+  }
+  void introduce() {
+    print(
+        "Hello, my name is $name and I'm $age years old.\nI'm $height cm tall and weigh $weight kg.");
+  }
 }
