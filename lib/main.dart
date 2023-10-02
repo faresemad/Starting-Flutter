f58949ca45d56068ef574a2796c18694ea1bfa2f
Fares Emad <faresemadx@gmail.com>
2023-10-02 14:03:27 +0200

    used `named parameters` and set `attributes` in constuctor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 void main() {
-  Humen fares = new Humen();
-  fares.name = "Fares Emad";
-  fares.age = 22;
-  fares.height = 183;
-  fares.weight = 60;
+  Humen fares = new Humen(
+    name: "Fares Emad",
+    age: 22,
+    height: 183,
+    weight: 60,
+  );
+  // fares.name = "Fares Emad";
+  // fares.age = 22;
+  // fares.height = 183;
+  // fares.weight = 60;
   fares.introduce();
 }
 
@@ -14,8 +19,12 @@ class Humen {
   double? weight;
   int numberOfArms = 2;
   // Constructor function (runs when the class is created)
-  Humen() {
+  Humen({String? name, int? age, double? height, double? weight}) {
     print("A new human has been created!");
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
   }
   void introduce() {
     print(
