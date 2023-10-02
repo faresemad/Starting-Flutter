ff98f77b2be319147a4f51975e2e4a93988c2b70
Fares Emad <faresemadx@gmail.com>
2023-10-02 16:59:02 +0200

    write a basic constructor

--- lib/human.dart
+++ lib/human.dart
@@ -4,14 +4,17 @@ class Human {
   double? height;
   double? weight;
   int _numberOfArms = 2; // Private variable
-
-  Human({required String name, int? age, double? height, double? weight}) {
-    print("A new human has been created!");
-    this.name = name;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-  }
+  // ----------------------------------------------------------------
+  // write a basic constructor
+  Human({this.name, this.age, this.height, this.weight});
+  // ----------------------------------------------------------------
+  // Human({required String name, int? age, double? height, double? weight}) {
+  //   print("A new human has been created!");
+  //   this.name = name;
+  //   this.age = age;
+  //   this.height = height;
+  //   this.weight = weight;
+  // }
   void introduce() {
     print(
         "Hello, my name is $name and I'm $age years old.\nI'm $height cm tall and weigh $weight kg.");
