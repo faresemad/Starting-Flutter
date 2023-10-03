8927bf3786464e56faf41b70c8f22efbf0abd16d
Fares Emad <faresemadx@gmail.com>
2023-10-03 16:11:19 +0200

    using and undersanding `Polymorphism`

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@
 import 'animal.dart';
 
 void main() {
-  print("Hello World");
   Dogs dog = Dogs(speed: "30mph", numOfLegs: 4);
-  print(dog.introduce());
+  Cats cat = Cats(speed: "20mph", numOfLegs: 4);
+  Lion lion = Lion(color: "Yellow", numOfLegs: 4);
+
+  List<Animal> animals = [dog, cat, lion];
+
+  for (Animal animal in animals) {
+    print(animal);
+  }
 }
 
 class Dogs extends Animal {
@@ -20,5 +26,35 @@ class Dogs extends Animal {
 
   introduce() {
     return speed + " => " + numOfLegs.toString();
+  }
+}
+
+class Cats extends Animal {
+  String speed;
+  Cats({required this.speed, required int numOfLegs})
+      : super(numOfLegs: numOfLegs);
+
+  @override
+  eat() {
+    print("Cats eat meat");
+  }
+
+  introduce() {
+    return speed + " => " + numOfLegs.toString();
+  }
+}
+
+class Lion extends Cats {
+  String color;
+  Lion({required this.color, required int numOfLegs})
+      : super(speed: "50mph", numOfLegs: numOfLegs);
+
+  @override
+  eat() {
+    print("Lion eat meat");
+  }
+
+  introduce() {
+    return color + " => " + speed + " => " + numOfLegs.toString();
   }
 }
