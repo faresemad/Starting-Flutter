4fbc3c0a4c41c390dbdb614ed782ca0269b21a59
Fares Emad <faresemadx@gmail.com>
2023-10-11 15:33:26 +0200

    feat: column widget

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -12,21 +12,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 84, 97, 104),
-      appBar: AppBar(
-        title: Center(child: Text('I Am Rich, `Fares Emad`')),
-        backgroundColor: Color.fromARGB(255, 43, 81, 100),
-      ),
-      body: Center(
-        child: Container(
-          color: const Color.fromARGB(255, 43, 53, 58),
-          height: 120,
-          width: 200,
-          child: Center(
-            child: Text("Hello Mother Fucker"),
-          ),
+        backgroundColor: Color.fromARGB(255, 84, 97, 104),
+        appBar: AppBar(
+          title: Center(child: Text('I Am Rich, `Fares Emad`')),
+          backgroundColor: Color.fromARGB(255, 43, 81, 100),
         ),
-      ),
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center, // by default
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image(
+                image: NetworkImage(
+                    'https://c4.wallpaperflare.com/wallpaper/365/244/884/uchiha-itachi-naruto-shippuuden-anbu-silhouette-wallpaper-preview.jpg'),
+              ),
+            ),
+            Text(
+              'Uchiha Itachi',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+          ],
+        ));
   }
 }
